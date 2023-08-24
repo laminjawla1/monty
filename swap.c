e5d1fa38d77910fdf568d0a7005a8ac53f8ef098
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-24 09:39:36 +0000

    swap

--- swap.c
+++ swap.c
@@ -13,6 +13,7 @@ void swap(stack_t **head, unsigned int current_line)
 	if (!*head || !(*head)->next)
 	{
 		fprintf(stderr, "%u: can't swap, stack too short\n", current_line);
+		shutdown();
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*head)->n;
