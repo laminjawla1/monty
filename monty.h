f6ba3d916eb641e1235f2c87d59f1ddb713c17f3
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-25 12:01:47 +0000

    rotl

--- monty.h
+++ monty.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 /*Global Macros*/
 #define ASCII_MIN 0
--- rotl.c
+++ rotl.c
@@ -4,23 +4,15 @@
 *rotl - Rotates the stack to the top.
 *
 *@head: Head of the linked list
-*@l: Current_line
+*@current_line: Current_line
 */
-void rotl(stack_t **head, unsigned int __attribute__((unused))l)
+void rotl(stack_t **head, unsigned int current_line)
 {
-	stack_t *start, *end;
 	int n;
 
-	start = end = *head;
-	while (end->next)
-		end = end->next;
-	while (start < end)
-	{
-		n = start->n;
-		start->n = end->n;
-		end->n = n;
-
-		start = start->next;
-		end = end->prev;
-	}
+	if (!*head || !(*head)->next)
+		return;
+	n = (*head)->n;
+	pop(head, current_line);
+	add_dnodeint_end(head, n);
 }
